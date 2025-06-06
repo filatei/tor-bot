dfc32c83b2c664d8625dfba6d0a6e8c0882e4da2
Akpodigha Filatei <filatei@gtsng.com>
2025-06-06 12:34:19 +0100

    reorgG

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import streamlit as st
 from app.dashboard import dashboard_tab
 from app.calculator import profit_calculator_tab
 
-tab1, tab2 = st.tabs(["📈 Trade Dashboard", "📊 Profit Calculator"])
+tab1, tab2 = st.tabs([" Profit Calculator", "📈 Trade Dashboard"])
 with tab1:
-    dashboard_tab()
-with tab2:
     profit_calculator_tab()
+with tab2:    
+    dashboard_tab()
 
 st.markdown("---")
 st.caption("© 2025 Torama. All rights reserved.")
